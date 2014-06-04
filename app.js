3fc782a14de104248fb3296416d3ae552c2c1468
Evan Moore <etmoore@gmail.com>
2014-06-03 23:16:06 -0600

    changed static todo text

---
+++ app.js
@@ -0,0 +1,68 @@
+
+
+//Keybindings
+var ENTER_KEY = 13;
+var ESCAPE_KEY = 27;
+
+// create an object that handles basic utility tasks
+/*
+var util = {
+  //create a unique id for each task
+  uuid: function(){},
+  //pluralize text when there are multiple items
+  pluralize: function(){},
+  //handle local storage
+  store: function(){}
+};
+*/
+
+$()
+
+
+/*
+var App = {
+  init: function(){},
+  cacheElements: function(){},
+  bindEvents: function(){},
+  render: function(){},
+  renderFooter: function(){},
+  toggleAll: function(){},
+  getActiveTodos: function(){},
+  getCompletedTodos: function(){},
+  getFilteredTodos: function(){},
+  destroyCompleted: function(){},
+  // accepts an element from inside the `.item` div and returns the corresponding index in the `todos` array
+  indexFromEl: function(){},
+  create: function(){},
+  toggle: function(){},
+  edit: function(){},
+  editKeyup: function(){},
+  update: function(){},
+  destroy: function(){},
+
+}
+*/
+
+//App.init();
+
+//Local Storage
+  //getting stuff in
+  //getting stuff out
+
+//Template
+  //clone
+  //insert text
+  //inserting into list
+
+//list
+  //sort
+    //All
+    //Active
+    //Completed
+  //Complete
+    //single
+    //All
+  //edit
+    //update
+  //add
+  //delete
